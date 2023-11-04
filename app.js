07b1e1b914a0ab1afc4dbae8fc948221b84f059f
Zhenya <karel.zhenya1257@mail.ru>
2023-11-04 12:48:41 +0300

    6.2

--- app.js
+++ app.js
@@ -2,11 +2,16 @@ const express = require('express');
 const mongoose = require('mongoose')
 const router = require('./routes');
 const { PORT = 3000} = process.env;
+const allowedCors = ['localhost:3000'];
 
 const app = express();
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 app.use(express.json());
 app.use((req, res, next) => {
+  const {origin} = req.headers;
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin)
+  }
   req.user = {
     _id: '654267bb926e4640df8ea202'
   };
