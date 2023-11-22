d6394c8f28554b1eb5c14f858cb1643c59f65951
Zhenya <karel.zhenya1257@mail.ru>
2023-11-22 15:02:25 +0300

    test

--- errors/errorsHandler.js
+++ errors/errorsHandler.js
@@ -1,9 +1,8 @@
 const errorHandler = (err, req, res, next) => {
-  //const statusCode = err.statusCode || 500;
-  //const message = statusCode === 500 ? 'На сервере произошла ошибка' : err.message;
-  //res.status(statusCode).send({ message });
-  res.status(500).send({ message: 'Ошибка сервера'})
-
+  const statusCode = err.statusCode || 500;
+  const message = statusCode === 500 ? 'На сервере произошла ошибка' : err.message;
+  res.status(statusCode).send({ message });
+  next();
 };
 
 module.exports = errorHandler;
